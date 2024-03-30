a01556160ccbead2332afe82ce112ed1d6ef3e10
danpier dos reis <d6nt3_d3m0n@outlook.com>
2024-03-30 11:28:33 -0300

    First Basic Logger with out libraries and using app.Config file

---
+++ Csharp Basic Logger/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+
+namespace Csharp_Basic_Logger
+{
+    public static class Logger
+    {
+        public static void WriteLog(string message)
+        {
+            string logPath = ConfigurationManager.AppSettings["logPathTxtFile"];
+
+            using (StreamWriter writer = new StreamWriter(logPath,true)){
+                writer.Write(message);
+            }
+        }
+    }
+}
---
+++ Csharp Basic Logger/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Csharp_Basic_Logger;
+
+string? message = Console.ReadLine();
+Logger.WriteLog(message == null ? "No hay mensaje":message);
